247a04048b406d884fb3df70be391e7c1109b457
Niranjan <niranjans3105@gmail.com>
2024-06-19 23:57:22 +0530

    COmmit on 19th June, different attributes

--- ML3.py
+++ ML3.py
@@ -7,17 +7,18 @@ dictionary = df.to_dict()
 #print(df)
 #print(df.columns)
 #print(df['total sulfur dioxide'])
-a = df['total sulfur dioxide']
-b = [0]*1744
-#print(a)
-plt.plot(a)
+a = df['free sulfur dioxide']
+b = df['fixed acidity']
+print(a)
+print(b)
+plt.plot(a,b)
 plt.show()
 print('sklearn : {}'.format(sklearn.__version__))
 #print(df.shape)
 #print(df.describe())
 #df.plot(kind ='box', subplots = True, layout = (12,12), sharex = False, sharey = False )
 #plt.show()
-print(df.values)
+#print(df.values)
 #print(df.shape())
 
 
